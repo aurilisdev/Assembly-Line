158238b31f4be945d29e982a179be1e99d507389
aurilisdev <aurilisdev@gmail.com>
2021-03-21 02:37:12 +0100

    Fix sounds + model register

--- src/main/java/assemblyline/client/ClientRegister.java
+++ src/main/java/assemblyline/client/ClientRegister.java
@@ -9,11 +9,26 @@ import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
 @OnlyIn(Dist.CLIENT)
+@EventBusSubscriber(modid = References.ID, bus = Bus.MOD)
 public class ClientRegister {
+    @SubscribeEvent
+    public static void onModelEvent(ModelRegistryEvent event) {
+	ModelLoader.addSpecialModel(MODEL_CONVEYOR);
+	ModelLoader.addSpecialModel(MODEL_CONVEYORANIMATED);
+	ModelLoader.addSpecialModel(MODEL_SLOPEDCONVEYOR);
+	ModelLoader.addSpecialModel(MODEL_SLOPEDCONVEYORANIMATED);
+	ModelLoader.addSpecialModel(MODEL_SLOPEDCONVEYORDOWN);
+	ModelLoader.addSpecialModel(MODEL_SLOPEDCONVEYORDOWNANIMATED);
+    }
+
     public static final ResourceLocation MODEL_CONVEYOR = new ResourceLocation(References.ID + ":block/conveyorbelt");
     public static final ResourceLocation MODEL_CONVEYORANIMATED = new ResourceLocation(References.ID + ":block/conveyorbeltrunning");
     public static final ResourceLocation MODEL_SLOPEDCONVEYOR = new ResourceLocation(References.ID + ":block/conveyorbeltsloped");
@@ -23,12 +38,6 @@ public class ClientRegister {
 	    References.ID + ":block/conveyorbeltslopeddownrunning");
 
     public static void setup() {
-	ModelLoader.addSpecialModel(MODEL_CONVEYOR);
-	ModelLoader.addSpecialModel(MODEL_CONVEYORANIMATED);
-	ModelLoader.addSpecialModel(MODEL_SLOPEDCONVEYOR);
-	ModelLoader.addSpecialModel(MODEL_SLOPEDCONVEYORANIMATED);
-	ModelLoader.addSpecialModel(MODEL_SLOPEDCONVEYORDOWN);
-	ModelLoader.addSpecialModel(MODEL_SLOPEDCONVEYORDOWNANIMATED);
 	ScreenManager.registerFactory(DeferredRegisters.CONTAINER_SORTERBELT.get(), ScreenSorterBelt::new);
 	ClientRegistry.bindTileEntityRenderer(DeferredRegisters.TILE_CRATE.get(), RenderCrate::new);
 	ClientRegistry.bindTileEntityRenderer(DeferredRegisters.TILE_BELT.get(), RenderConveyorBelt::new);
