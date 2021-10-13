cf715e2ecbbd5fd3636a38d9ee0770e0b56bb759
aurilisdev <aurilisdev@gmail.com>
2021-10-13 18:23:48 +0200

    Fix rotational logic and position

--- src/main/java/assemblyline/client/render/tile/RenderElevatorBelt.java
+++ src/main/java/assemblyline/client/render/tile/RenderElevatorBelt.java
@@ -6,6 +6,7 @@ import assemblyline.client.ClientRegister;
 import assemblyline.common.tile.TileConveyorBelt;
 import assemblyline.common.tile.TileElevatorBelt;
 import electrodynamics.prefab.tile.components.ComponentType;
+import electrodynamics.prefab.tile.components.type.ComponentDirection;
 import electrodynamics.prefab.tile.components.type.ComponentInventory;
 import electrodynamics.prefab.utilities.UtilitiesRendering;
 import net.minecraft.client.Minecraft;
@@ -61,16 +62,60 @@ public class RenderElevatorBelt extends TileEntityRenderer<TileElevatorBelt> {
 	if (!(next instanceof TileConveyorBelt)) {
 	    progressModifier -= 0.5;
 	}
+	Direction dir = tile.<ComponentDirection>getComponent(ComponentType.Direction).getDirection();
 	if (totalSlotsUsed > 0) {
 	    for (int i = 0; i < inv.getSizeInventory(); i++) {
 		ItemStack stack = inv.getStackInSlot(i);
 		if (totalSlotsUsed == 1) {
 		    matrixStackIn.push();
-		    matrixStackIn.translate(0.5, progressModifier, 0.5);
+		    matrixStackIn.translate(0, stack.getItem() instanceof BlockItem ? 0.48 : 0.33, 0);
+		    matrixStackIn.translate(0.5 + dir.getOpposite().getXOffset() * 0.05, progressModifier,
+			    0.5 + dir.getOpposite().getZOffset() * 0.05);
+		    if (dir == Direction.NORTH) {
+			matrixStackIn.rotate(Vector3f.YP.rotationDegrees(180));
+		    } else if (dir == Direction.EAST) {
+			matrixStackIn.rotate(Vector3f.YP.rotationDegrees(90));
+		    } else if (dir == Direction.WEST) {
+			matrixStackIn.rotate(Vector3f.YP.rotationDegrees(-90));
+		    }
 		    matrixStackIn.scale(0.35f, 0.35f, 0.35f);
 		    if (!(stack.getItem() instanceof BlockItem)) {
 			matrixStackIn.rotate(Vector3f.XN.rotationDegrees(90));
 		    }
+		    int rotate = 90;
+		    matrixStackIn.rotate(dir == Direction.NORTH ? Vector3f.ZN.rotationDegrees(rotate)
+			    : dir == Direction.SOUTH ? Vector3f.ZP.rotationDegrees(-rotate)
+				    : dir == Direction.WEST ? Vector3f.ZN.rotationDegrees(rotate) : Vector3f.ZP.rotationDegrees(-rotate));
+		    matrixStackIn.rotate(Vector3f.XN.rotationDegrees(-90));
+		    Minecraft.getInstance().getItemRenderer().renderItem(stack, TransformType.NONE, combinedLightIn, combinedOverlayIn, matrixStackIn,
+			    bufferIn);
+		    matrixStackIn.pop();
+		} else if (totalSlotsUsed == 2) {
+		    matrixStackIn.push();
+		    matrixStackIn.translate(0, stack.getItem() instanceof BlockItem ? 0.48 : 0.33, 0);
+		    matrixStackIn.translate(0.5 + dir.getOpposite().getXOffset() * 0.05, progressModifier,
+			    0.5 + dir.getOpposite().getZOffset() * 0.05);
+		    if (dir == Direction.NORTH) {
+			matrixStackIn.translate((i == 0 ? 0.25 : 0.75) - 0.5, 0, 0);
+			matrixStackIn.rotate(Vector3f.YP.rotationDegrees(180));
+		    } else if (dir == Direction.EAST) {
+			matrixStackIn.translate(0, 0, (i == 0 ? 0.25 : 0.75) - 0.5);
+			matrixStackIn.rotate(Vector3f.YP.rotationDegrees(90));
+		    } else if (dir == Direction.WEST) {
+			matrixStackIn.translate(0, 0, (i == 0 ? 0.25 : 0.75) - 0.5);
+			matrixStackIn.rotate(Vector3f.YP.rotationDegrees(-90));
+		    } else if (dir == Direction.SOUTH) {
+			matrixStackIn.translate((i == 0 ? 0.25 : 0.75) - 0.5, 0, 0);
+		    }
+		    matrixStackIn.scale(0.35f, 0.35f, 0.35f);
+		    if (!(stack.getItem() instanceof BlockItem)) {
+			matrixStackIn.rotate(Vector3f.XN.rotationDegrees(90));
+		    }
+		    int rotate = 90;
+		    matrixStackIn.rotate(dir == Direction.NORTH ? Vector3f.ZN.rotationDegrees(rotate)
+			    : dir == Direction.SOUTH ? Vector3f.ZP.rotationDegrees(-rotate)
+				    : dir == Direction.WEST ? Vector3f.ZN.rotationDegrees(rotate) : Vector3f.ZP.rotationDegrees(-rotate));
+		    matrixStackIn.rotate(Vector3f.XN.rotationDegrees(-90));
 		    Minecraft.getInstance().getItemRenderer().renderItem(stack, TransformType.NONE, combinedLightIn, combinedOverlayIn, matrixStackIn,
 			    bufferIn);
 		    matrixStackIn.pop();
