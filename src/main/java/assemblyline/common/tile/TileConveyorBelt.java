710f3bbdc8ddd2de78ce1c1e5dea804d09f0a152
aurilisdev <aurilisdev@gmail.com>
2021-11-15 22:53:38 +0100

    Update build

--- src/main/java/assemblyline/common/tile/TileConveyorBelt.java
+++ src/main/java/assemblyline/common/tile/TileConveyorBelt.java
@@ -217,9 +217,11 @@ public class TileConveyorBelt extends GenericTileTicking {
 	    if (currentSpread == 0 || currentSpread == 16) {
 		if (electro.getJoulesStored() < Constants.CONVEYORBELT_USAGE) {
 		    currentSpread = 0;
+		    this.<ComponentPacketHandler>getComponent(ComponentType.PacketHandler).sendGuiPacketToTracking();
 		} else {
 		    electro.joules(electro.getJoulesStored() - Constants.CONVEYORBELT_USAGE);
 		    currentSpread = 16;
+		    this.<ComponentPacketHandler>getComponent(ComponentType.PacketHandler).sendGuiPacketToTracking();
 		}
 	    }
 	}
