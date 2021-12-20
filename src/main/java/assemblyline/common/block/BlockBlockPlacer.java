641ef6e36ead3e79203630248dee2e1aebfd89f2
Sebastian Siljuholtet Johansen <aurilisdev@gmail.com>
2021-12-20 01:58:54 +0100

    Add blockplacer model

--- src/main/java/assemblyline/common/block/BlockBlockPlacer.java
+++ src/main/java/assemblyline/common/block/BlockBlockPlacer.java
@@ -2,17 +2,10 @@ package assemblyline.common.block;
 
 import assemblyline.common.tile.TileBlockPlacer;
 import electrodynamics.prefab.block.GenericMachineBlock;
-import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.state.BlockState;
 
 public class BlockBlockPlacer extends GenericMachineBlock {
 
 	public BlockBlockPlacer() {
 		super(TileBlockPlacer::new);
-	}
-
-	@Override
-	public RenderShape getRenderShape(BlockState state) {
-		return RenderShape.ENTITYBLOCK_ANIMATED;
 	}
 }
