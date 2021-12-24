ba1506be8250280b3a0ef5325ca69027b1a1aef5
Sebastian Siljuholtet Johansen <aurilisdev@gmail.com>
2021-12-25 00:53:52 +0100

    Update to new improved screen system

--- src/main/java/assemblyline/client/screen/ScreenSorterBelt.java
+++ src/main/java/assemblyline/client/screen/ScreenSorterBelt.java
@@ -1,23 +1,11 @@
 package assemblyline.client.screen;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import assemblyline.common.inventory.container.ContainerSorterBelt;
 import assemblyline.common.settings.Constants;
-import assemblyline.common.tile.TileSorterBelt;
-import electrodynamics.api.electricity.formatting.ChatFormatter;
-import electrodynamics.api.electricity.formatting.ElectricUnit;
 import electrodynamics.prefab.screen.GenericScreen;
 import electrodynamics.prefab.screen.component.ScreenComponentElectricInfo;
 import electrodynamics.prefab.screen.component.ScreenComponentInfo;
-import electrodynamics.prefab.tile.components.ComponentType;
-import electrodynamics.prefab.tile.components.type.ComponentElectrodynamic;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -26,22 +14,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public class ScreenSorterBelt extends GenericScreen<ContainerSorterBelt> {
 	public ScreenSorterBelt(ContainerSorterBelt container, Inventory playerInventory, Component title) {
 		super(container, playerInventory, title);
-		components.add(new ScreenComponentElectricInfo(this::getEnergyInformation, this, -ScreenComponentInfo.SIZE + 1, 2));
+		components.add(
+				new ScreenComponentElectricInfo(this, -ScreenComponentInfo.SIZE + 1, 2).tag("sorterbelt").wattage(Constants.SORTERBELT_USAGE * 20));
 	}
-
-	private List<? extends FormattedCharSequence> getEnergyInformation() {
-		ArrayList<FormattedCharSequence> list = new ArrayList<>();
-		TileSorterBelt box = menu.getHostFromIntArray();
-		if (box != null) {
-			ComponentElectrodynamic electro = box.getComponent(ComponentType.Electrodynamic);
-			list.add(new TranslatableComponent("gui.sorterbelt.usage",
-					new TextComponent(ChatFormatter.getElectricDisplayShort(Constants.SORTERBELT_USAGE * 20, ElectricUnit.WATT))
-							.withStyle(ChatFormatting.GRAY)).withStyle(ChatFormatting.DARK_GRAY).getVisualOrderText());
-			list.add(new TranslatableComponent("gui.sorterbelt.voltage",
-					new TextComponent(ChatFormatter.getElectricDisplayShort(electro.getVoltage(), ElectricUnit.VOLTAGE))
-							.withStyle(ChatFormatting.GRAY)).withStyle(ChatFormatting.DARK_GRAY).getVisualOrderText());
-		}
-		return list;
-	}
-
 }
