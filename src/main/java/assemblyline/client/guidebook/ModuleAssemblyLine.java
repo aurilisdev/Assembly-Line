fab9b3d1d7b68359402e76944fef10324f2c3646
skip999 <skiprocks999@gmail.com>
2022-03-22 10:31:26 -0500

    guidebook work

---
+++ src/main/java/assemblyline/client/guidebook/ModuleAssemblyLine.java
@@ -0,0 +1,42 @@
+package assemblyline.client.guidebook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import assemblyline.References;
+import assemblyline.client.guidebook.chapters.ChapterConveyers;
+import assemblyline.client.guidebook.chapters.ChapterMachines;
+import assemblyline.client.guidebook.chapters.ChapterOtherBlocks;
+import electrodynamics.client.guidebook.utils.ImageWrapperObject;
+import electrodynamics.client.guidebook.utils.components.Chapter;
+import electrodynamics.client.guidebook.utils.components.Module;
+
+public class ModuleAssemblyLine extends Module {
+
+	private static final ImageWrapperObject LOGO = new ImageWrapperObject(10, 38, 0, 0, 32, 32, 32, 32, References.ID + ":textures/screen/guidebook/assemblylinelogo.png");
+	
+	@Override
+	protected List<Chapter> genChapters() {
+		List<Chapter> chapters = new ArrayList<>();
+		chapters.add(new ChapterConveyers());
+		chapters.add(new ChapterMachines());
+		chapters.add(new ChapterOtherBlocks());
+		return chapters;
+	}
+
+	@Override
+	public ImageWrapperObject getLogo() {
+		return LOGO;
+	}
+
+	@Override
+	public String getTitleCat() {
+		return References.ID;
+	}
+
+	@Override
+	public boolean isFirst() {
+		return false;
+	}
+
+}
---
+++ src/main/java/assemblyline/client/guidebook/chapters/ChapterConveyers.java
@@ -0,0 +1,33 @@
+package assemblyline.client.guidebook.chapters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import assemblyline.DeferredRegisters;
+import electrodynamics.api.item.ItemUtils;
+import electrodynamics.client.guidebook.utils.ItemWrapperObject;
+import electrodynamics.client.guidebook.utils.components.Chapter;
+import electrodynamics.client.guidebook.utils.components.Page;
+
+public class ChapterConveyers extends Chapter {
+
+	private static final ItemWrapperObject LOGO = new ItemWrapperObject(17, 60, 2.0F, ItemUtils.fromBlock(DeferredRegisters.blockConveyorBelt));
+	
+	@Override
+	protected List<Page> genPages() {
+		List<Page> pages = new ArrayList<>();
+		pages.add(new Page());
+		return pages;
+	}
+
+	@Override
+	public ItemWrapperObject getLogo() {
+		return LOGO;
+	}
+
+	@Override
+	public String getTitleKey() {
+		return "guidebook.assemblyline.chapter.conveyers";
+	}
+
+}
