02cfe901ebe4b36585ce6176e130e405948ce434
aurilisdev <aurilisdev@gmail.com>
2022-08-11 01:53:56 +0200

    Update to newest forge/jei

--- src/main/java/assemblyline/client/ClientEvents.java
+++ src/main/java/assemblyline/client/ClientEvents.java
@@ -16,7 +16,8 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RenderLevelLastEvent;
+import net.minecraftforge.client.event.RenderLevelStageEvent;
+import net.minecraftforge.client.event.RenderLevelStageEvent.Stage;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -27,31 +28,33 @@ public class ClientEvents {
 	public static HashMap<BlockPos, Pair<List<List<Integer>>, List<AABB>>> farmerLines = new HashMap<>();
 
 	@SubscribeEvent
-	public static void renderSelectedBlocks(RenderLevelLastEvent event) {
-		PoseStack matrix = event.getPoseStack();
-		MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
-		VertexConsumer builder = buffer.getBuffer(RenderType.LINES);
-		Vec3 camera = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
-		for (Entry<BlockPos, AABB> en : outlines.entrySet()) {
-			AABB box = en.getValue().deflate(0.001);
-			matrix.pushPose();
-			matrix.translate(-camera.x, -camera.y, -camera.z);
-			LevelRenderer.renderLineBox(matrix, builder, box, 1.0F, 1.0F, 1.0F, 1.0F);
-			matrix.popPose();
-		}
-		for (Entry<BlockPos, Pair<List<List<Integer>>, List<AABB>>> en : farmerLines.entrySet()) {
-			List<List<Integer>> rgbaValues = en.getValue().getFirst();
-			List<AABB> lines = en.getValue().getSecond();
-			for (int i = 0; i < lines.size(); i++) {
-				AABB box = lines.get(i).deflate(0.01);
-				List<Integer> rgba = rgbaValues.get(i);
+	public static void renderSelectedBlocks(RenderLevelStageEvent event) {
+		if (event.getStage() == Stage.AFTER_WEATHER) {
+			PoseStack matrix = event.getPoseStack();
+			MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
+			VertexConsumer builder = buffer.getBuffer(RenderType.LINES);
+			Vec3 camera = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
+			for (Entry<BlockPos, AABB> en : outlines.entrySet()) {
+				AABB box = en.getValue().deflate(0.001);
 				matrix.pushPose();
 				matrix.translate(-camera.x, -camera.y, -camera.z);
-				LevelRenderer.renderLineBox(matrix, builder, box, rgba.get(1) / 255.0F, rgba.get(2) / 255.0F, rgba.get(3) / 255.0F, rgba.get(0) / 255.0F);
+				LevelRenderer.renderLineBox(matrix, builder, box, 1.0F, 1.0F, 1.0F, 1.0F);
 				matrix.popPose();
 			}
+			for (Entry<BlockPos, Pair<List<List<Integer>>, List<AABB>>> en : farmerLines.entrySet()) {
+				List<List<Integer>> rgbaValues = en.getValue().getFirst();
+				List<AABB> lines = en.getValue().getSecond();
+				for (int i = 0; i < lines.size(); i++) {
+					AABB box = lines.get(i).deflate(0.01);
+					List<Integer> rgba = rgbaValues.get(i);
+					matrix.pushPose();
+					matrix.translate(-camera.x, -camera.y, -camera.z);
+					LevelRenderer.renderLineBox(matrix, builder, box, rgba.get(1) / 255.0F, rgba.get(2) / 255.0F, rgba.get(3) / 255.0F, rgba.get(0) / 255.0F);
+					matrix.popPose();
+				}
+			}
+			buffer.endBatch(RenderType.LINES);
 		}
-		buffer.endBatch(RenderType.LINES);
 	}
 
 }
