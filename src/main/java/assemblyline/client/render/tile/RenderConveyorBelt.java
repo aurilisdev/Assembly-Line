0d7b546837fc7254016ceedebb75d7985d4201ea
skip999 <skiprocks999@gmail.com>
2025-06-01 13:22:04 -0500

    backport tweaks

--- src/main/java/assemblyline/client/render/tile/RenderConveyorBelt.java
+++ src/main/java/assemblyline/client/render/tile/RenderConveyorBelt.java
@@ -59,7 +59,7 @@ public class RenderConveyorBelt extends AbstractTileRenderer<TileConveyorBelt> {
 
 			}
 
-			move.mul(1.0F / 16.0F);
+			move.mul(partialTicks / 16.0f); // Must add tile speed later aka multiply by properties.conveyorClass.speed
 
 			if (tile.running.getValue()) {
 
@@ -73,19 +73,13 @@ public class RenderConveyorBelt extends AbstractTileRenderer<TileConveyorBelt> {
 
 			case HORIZONTAL:
 
-				matrixStackIn.translate(itemVec.x(), itemVec.y() + (blockItem ? 0.167 : 5.0f / 16.0f) + move.y(), itemVec.z());
+				matrixStackIn.translate(itemVec.x(), itemVec.y() + (blockItem ? 0.167 : 0.05f / 16.0f) + move.y(), itemVec.z());
 
 				matrixStackIn.scale(0.35f, 0.35f, 0.35f);
 
 				matrixStackIn.translate(0, 5.0f / (16.0f * 0.35f), 0);
 
 				if (!blockItem) {
-
-					matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(90));
-
-				}
-
-				if (direct == Direction.EAST || direct == Direction.WEST) {
 					matrixStackIn.mulPose(Vector3f.YN.rotationDegrees(90));
 				}
 
@@ -93,13 +87,14 @@ public class RenderConveyorBelt extends AbstractTileRenderer<TileConveyorBelt> {
 
 			case SLOPED_DOWN:
 
-				matrixStackIn.translate(itemVec.x(), itemVec.y() + (blockItem ? 0.167 : 2.0f / 16.0f), itemVec.z());
+				matrixStackIn.translate(itemVec.x(), itemVec.y() + (blockItem ? 0.167 : 1.0f / 16.0f), itemVec.z());
 
 				matrixStackIn.scale(0.35f, 0.35f, 0.35f);
 
 				if (!blockItem) {
 
-					matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(90));
+					matrixStackIn.mulPose(Vector3f.YN.rotationDegrees(90));
+					// matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(90));
 
 				}
 
@@ -109,29 +104,33 @@ public class RenderConveyorBelt extends AbstractTileRenderer<TileConveyorBelt> {
 
 					matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(180));
 					matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(rotate));
-					// matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(180));
-
+					if (!blockItem) {
+						matrixStackIn.translate(0, 0, -0.3);
+					}
 				} else if (direct == Direction.EAST) {
-
-					matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(90));
-					matrixStackIn.mulPose(Vector3f.XP.rotationDegrees(-rotate));
-					// matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(90));
-
+					if (blockItem) {
+						matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(90));
+						matrixStackIn.mulPose(Vector3f.XP.rotationDegrees(-rotate));
+					} else {
+						matrixStackIn.translate(0, -0.4, 0.4);
+						matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(45));
+					}
 				} else if (direct == Direction.WEST) {
-
-					matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(-90));
-					matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(rotate));
-					// matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(-90));
+					if (blockItem) {
+						matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(-90));
+						matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(rotate));
+					} else {
+						matrixStackIn.translate(0, -0.3, 0.5);
+						matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(-45));
+					}
 
 				} else if (direct == Direction.SOUTH) {
 
 					matrixStackIn.mulPose(Vector3f.XP.rotationDegrees(-rotate));
-
+					if (!blockItem) {
+						matrixStackIn.translate(0, 0, 0.4);
+					}
 				}
-
-				// matrixStackIn.mulPose(direct == Direction.NORTH ? Vector3f.XN.rotationDegrees(rotate) : direct == Direction.SOUTH ?
-				// Vector3f.XP.rotationDegrees(-rotate) : direct == Direction.WEST ? Vector3f.XN.rotationDegrees(rotate) :
-				// Vector3f.XP.rotationDegrees(-rotate));
 
 				matrixStackIn.translate(0, 2.0f / (16.0f * 0.35f), 0);
 
@@ -139,13 +138,14 @@ public class RenderConveyorBelt extends AbstractTileRenderer<TileConveyorBelt> {
 
 			case SLOPED_UP:
 
-				matrixStackIn.translate(itemVec.x(), itemVec.y() + (blockItem ? 0.167 : 7.0f / 16.0f), itemVec.z());
+				matrixStackIn.translate(itemVec.x(), itemVec.y() + (blockItem ? 0.4 : 7.0f / 16.0f), itemVec.z());
 
 				matrixStackIn.scale(0.35f, 0.35f, 0.35f);
 
 				if (!blockItem) {
 
-					matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(90));
+					matrixStackIn.mulPose(Vector3f.YN.rotationDegrees(90));
+					// matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(90));
 
 				}
 
@@ -156,23 +156,33 @@ public class RenderConveyorBelt extends AbstractTileRenderer<TileConveyorBelt> {
 					matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(180));
 					matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(rotate));
 					// matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(180));
-
+					if (!blockItem) {
+						matrixStackIn.translate(0, 0, -0.2);
+					}
 				} else if (direct == Direction.EAST) {
-
-					matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(90));
-					matrixStackIn.mulPose(Vector3f.XP.rotationDegrees(-rotate));
-					// matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(90));
-
+					if (blockItem) {
+						matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(90));
+						matrixStackIn.mulPose(Vector3f.XP.rotationDegrees(-rotate));
+					} else {
+						matrixStackIn.translate(0, -0.9, 0.3);
+						matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(-45));
+					}
 				} else if (direct == Direction.WEST) {
 
-					matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(-90));
-					matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(rotate));
-					// matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(-90));
+					if (blockItem) {
+						matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(-90));
+						matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(rotate));
+					} else {
+						matrixStackIn.translate(0, -1, 0.4);
+						matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(45));
+					}
 
 				} else if (direct == Direction.SOUTH) {
 
 					matrixStackIn.mulPose(Vector3f.XP.rotationDegrees(-rotate));
-
+					if (!blockItem) {
+						matrixStackIn.translate(0, 0, 0.2);
+					}
 				}
 
 				// matrixStackIn.mulPose(direct == Direction.NORTH ? Vector3f.XN.rotationDegrees(rotate) : direct == Direction.SOUTH ?
@@ -191,7 +201,8 @@ public class RenderConveyorBelt extends AbstractTileRenderer<TileConveyorBelt> {
 
 				if (!blockItem) {
 
-					matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(90));
+					matrixStackIn.mulPose(Vector3f.YN.rotationDegrees(90));
+					// matrixStackIn.mulPose(Vector3f.XN.rotationDegrees(90));
 
 				}
 
