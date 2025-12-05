b56e804a7338523a3fa49b7a14896ca48551d192
aurilisdev <aurilisdev@gmail.com>
2025-12-05 15:24:53 +0100

    Fix #81
    Re: https://github.com/aurilisdev/Assembly-Line/issues/81

--- src/main/java/assemblyline/common/tile/TileBlockPlacer.java
+++ src/main/java/assemblyline/common/tile/TileBlockPlacer.java
@@ -6,14 +6,11 @@ import assemblyline.common.tile.util.TileOutlineArea;
 import assemblyline.registers.AssemblyLineTiles;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.context.DirectionalPlaceContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.Vec3;
 import voltaic.common.item.ItemUpgrade;
 import voltaic.prefab.properties.types.PropertyTypes;
 import voltaic.prefab.properties.variant.SingleProperty;
@@ -29,76 +26,86 @@ import voltaic.registers.VoltaicCapabilities;
 
 public class TileBlockPlacer extends TileOutlineArea {
 
-    public SingleProperty<Integer> ticksSinceCheck = property(new SingleProperty<>(PropertyTypes.INTEGER, "ticksSinceCheck", 0));
-    public SingleProperty<Integer> currentWaitTime = property(new SingleProperty<>(PropertyTypes.INTEGER, "currentWaitTime", 0));
+    public SingleProperty<Integer> ticksSinceCheck = property(
+	    new SingleProperty<>(PropertyTypes.INTEGER, "ticksSinceCheck", 0));
+    public SingleProperty<Integer> currentWaitTime = property(
+	    new SingleProperty<>(PropertyTypes.INTEGER, "currentWaitTime", 0));
 
     public TileBlockPlacer(BlockPos pos, BlockState state) {
-        super(AssemblyLineTiles.TILE_BLOCKPLACER.get(), pos, state);
-        addComponent(new ComponentPacketHandler(this));
-        addComponent(new ComponentTickable(this).tickServer(this::tickServer));
-        addComponent(new ComponentElectrodynamic(this, false, true).setInputDirections(BlockEntityUtils.MachineDirection.FRONT).voltage(VoltaicCapabilities.DEFAULT_VOLTAGE).maxJoules(AssemblyLineConfig.INSTANCE.BLOCKPLACER_USAGE.getAsDouble() * 2));
-        addComponent(new ComponentInventory(this, ComponentInventory.InventoryBuilder.newInv().inputs(1).upgrades(3))
-                //
-                .setDirectionsBySlot(0, BlockEntityUtils.MachineDirection.TOP, BlockEntityUtils.MachineDirection.BOTTOM, BlockEntityUtils.MachineDirection.LEFT, BlockEntityUtils.MachineDirection.RIGHT).validUpgrades(ContainerBlockPlacer.VALID_UPGRADES).valid(machineValidator()));
-        addComponent(new ComponentContainerProvider("blockplacer", this).createMenu((id, player) -> new ContainerBlockPlacer(id, player, getComponent(IComponentType.Inventory), getCoordsArray())));
-        addComponent(new ComponentForgeEnergy(this));
-        height.setValue(1);
+	super(AssemblyLineTiles.TILE_BLOCKPLACER.get(), pos, state);
+	addComponent(new ComponentPacketHandler(this));
+	addComponent(new ComponentTickable(this).tickServer(this::tickServer));
+	addComponent(new ComponentElectrodynamic(this, false, true)
+		.setInputDirections(BlockEntityUtils.MachineDirection.FRONT)
+		.voltage(VoltaicCapabilities.DEFAULT_VOLTAGE)
+		.maxJoules(AssemblyLineConfig.INSTANCE.BLOCKPLACER_USAGE.getAsDouble() * 2));
+	addComponent(new ComponentInventory(this, ComponentInventory.InventoryBuilder.newInv().inputs(1).upgrades(3))
+		//
+		.setDirectionsBySlot(0, BlockEntityUtils.MachineDirection.TOP, BlockEntityUtils.MachineDirection.BOTTOM,
+			BlockEntityUtils.MachineDirection.LEFT, BlockEntityUtils.MachineDirection.RIGHT)
+		.validUpgrades(ContainerBlockPlacer.VALID_UPGRADES).valid(machineValidator()));
+	addComponent(new ComponentContainerProvider("blockplacer", this)
+		.createMenu((id, player) -> new ContainerBlockPlacer(id, player, getComponent(IComponentType.Inventory),
+			getCoordsArray())));
+	addComponent(new ComponentForgeEnergy(this));
+	height.setValue(1);
     }
 
     public void tickServer(ComponentTickable tickable) {
-        ComponentInventory inv = getComponent(IComponentType.Inventory);
-        ComponentElectrodynamic electro = getComponent(IComponentType.Electrodynamic);
+	ComponentInventory inv = getComponent(IComponentType.Inventory);
+	ComponentElectrodynamic electro = getComponent(IComponentType.Electrodynamic);
 
-        // we can add speed upgrade functionality if you want
-        currentWaitTime.setValue(20);
+	// we can add speed upgrade functionality if you want
+	currentWaitTime.setValue(20);
 
-        for (ItemStack stack : inv.getUpgradeContents()) {
-            if (!stack.isEmpty()) {
-                ItemUpgrade upgrade = (ItemUpgrade) stack.getItem();
-                switch (upgrade.subtype) {
-                    case iteminput:
-                        upgrade.subtype.applyUpgrade.accept(this, stack, 0);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
+	for (ItemStack stack : inv.getUpgradeContents()) {
+	    if (!stack.isEmpty()) {
+		ItemUpgrade upgrade = (ItemUpgrade) stack.getItem();
+		switch (upgrade.subtype) {
+		case iteminput:
+		    upgrade.subtype.applyUpgrade.accept(this, stack, 0);
+		    break;
+		default:
+		    break;
+		}
+	    }
+	}
 
+	if (electro.getJoulesStored() < AssemblyLineConfig.INSTANCE.BLOCKPLACER_USAGE.getAsDouble()
+		|| inv.areInputsEmpty()) {
+	    return;
+	}
 
-        if (electro.getJoulesStored() < AssemblyLineConfig.INSTANCE.BLOCKPLACER_USAGE.getAsDouble() || inv.areInputsEmpty()) {
-            return;
-        }
+	ticksSinceCheck.setValue(ticksSinceCheck.getValue() + 1);
 
-        ticksSinceCheck.setValue(ticksSinceCheck.getValue() + 1);
+	if (ticksSinceCheck.getValue() >= currentWaitTime.getValue()) {
+	    ticksSinceCheck.setValue(0);
+	}
 
-        if (ticksSinceCheck.getValue() >= currentWaitTime.getValue()) {
-            ticksSinceCheck.setValue(0);
-        }
+	if (ticksSinceCheck.getValue() != 0) {
+	    return;
+	}
 
-        if (ticksSinceCheck.getValue() != 0) {
-            return;
-        }
+	Direction facing = getFacing();
+	BlockPos off = worldPosition.offset(facing.getOpposite().getNormal());
+	BlockState state = level.getBlockState(off);
+	electro.setJoulesStored(
+		electro.getJoulesStored() - AssemblyLineConfig.INSTANCE.BLOCKBREAKER_USAGE.getAsDouble());
+	if (!state.isAir()) {
+	    return;
+	}
 
-        Direction facing = getFacing();
-        BlockPos off = worldPosition.offset(facing.getOpposite().getNormal());
-        BlockState state = level.getBlockState(off);
-        electro.setJoulesStored(electro.getJoulesStored() - AssemblyLineConfig.INSTANCE.BLOCKBREAKER_USAGE.getAsDouble());
-        if (!state.isAir()) {
-            return;
-        }
+	ItemStack stack = inv.getItem(0);
 
-        ItemStack stack = inv.getItem(0);
-
-        if (!stack.isEmpty() && stack.getItem() instanceof BlockItem bi) {
-            Block b = bi.getBlock();
-            BlockState newState = b.getStateForPlacement(new BlockPlaceContext(level, null, InteractionHand.MAIN_HAND, stack, new BlockHitResult(Vec3.ZERO, facing, off, false)));
-            if (newState.canSurvive(level, off)) {
-                level.setBlockAndUpdate(off, newState);
-                stack.shrink(1);
-            }
-        }
-
+	if (!stack.isEmpty() && stack.getItem() instanceof BlockItem bi) {
+	    Block b = bi.getBlock();
+	    BlockState newState = b
+		    .getStateForPlacement(new DirectionalPlaceContext(level, off, facing, stack, facing));
+	    if (newState.canSurvive(level, off)) {
+		level.setBlockAndUpdate(off, newState);
+		stack.shrink(1);
+	    }
+	}
 
     }
 
