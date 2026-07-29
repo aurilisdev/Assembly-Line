e0e4fd762a7e5c2d9146684198962eecc6681431
aurilisdev <aurilisdev@gmail.com>
2026-07-29 20:00:19 +0200

    Patch 2: Backport all fixes from 1.21.1

--- src/main/java/assemblyline/client/render/tile/RenderConveyorBelt.java
+++ src/main/java/assemblyline/client/render/tile/RenderConveyorBelt.java
@@ -238,12 +238,18 @@ public class RenderConveyorBelt extends AbstractTileRenderer<TileConveyorBelt> {
 		case SLOPED_UP -> tile.running.getValue() ? AssemblyLineClientRegister.MODEL_SLOPEDCONVEYORUPANIMATED : AssemblyLineClientRegister.MODEL_SLOPEDCONVEYORUP;
 		case VERTICAL -> {
 
-			if (tile.getLevel().getBlockEntity(tile.getBlockPos().below()) instanceof GenericTileConveyorBelt belt && belt.getConveyorType() == ConveyorType.VERTICAL) {
+                if (tile.getLevel().getBlockEntity(tile.getBlockPos().below()) instanceof GenericTileConveyorBelt belt && belt.getConveyorType() == ConveyorType.VERTICAL) {
+                    if(!(tile.getLevel().getBlockEntity(tile.getBlockPos().above()) instanceof GenericTileConveyorBelt)){
+    			yield tile.running.getValue() ? AssemblyLineClientRegister.MODEL_ELEVATORRUNNINGBUTLAST : AssemblyLineClientRegister.MODEL_ELEVATORBUTLAST;
 
-				yield tile.running.getValue() ? AssemblyLineClientRegister.MODEL_ELEVATORRUNNING : AssemblyLineClientRegister.MODEL_ELEVATOR;
+                    }
+                    yield tile.running.getValue() ? AssemblyLineClientRegister.MODEL_ELEVATORRUNNING : AssemblyLineClientRegister.MODEL_ELEVATOR;
 
-			}
-			yield tile.running.getValue() ? AssemblyLineClientRegister.MODEL_ELEVATORBOTTOMRUNNING : AssemblyLineClientRegister.MODEL_ELEVATORBOTTOM;
+                } else if(!(tile.getLevel().getBlockEntity(tile.getBlockPos().above()) instanceof GenericTileConveyorBelt)){
+    			yield tile.running.getValue() ? AssemblyLineClientRegister.MODEL_ELEVATORBOTTOMRUNNINGBUTLAST : AssemblyLineClientRegister.MODEL_ELEVATORBOTTOMBUTLAST;
+
+                }
+		yield tile.running.getValue() ? AssemblyLineClientRegister.MODEL_ELEVATORBOTTOMRUNNING : AssemblyLineClientRegister.MODEL_ELEVATORBOTTOM;
 
 		}
 		default -> tile.running.getValue() ? AssemblyLineClientRegister.MODEL_CONVEYORANIMATED : AssemblyLineClientRegister.MODEL_CONVEYOR;
